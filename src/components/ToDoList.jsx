13aa245cb5dd7449591422fd5eacf05cc28ddb21
keiinuki <username@example.com>
2021-07-23 09:34:10 +0900

    更新

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,17 +3,29 @@ import { CompleteButton } from "./CompleteButton";
 import { DeleteButton } from "./DeleteButton";
 
 export const ToDoList = (props) => {
-  const [arr, setArr] = useState(["花に水をやる"]);
+  const [arr, setArr] = useState(["例）花に水をやる"]);
   const [word, setWord] = useState("");
 
 
-  const onClickAdd = () => {    
-    const newWord = [...word];
-    newWord.push(setWord);
+    const onClickAdd = (e) => {
+    e.preventDefault()    
+    //const newWord = [...word];
+    //console.log(word);
+    //newWord.push(setWord);
     const newArr = [...arr];
-    newArr.push(newWord);
+    newArr.push(word);
     setArr(newArr);
+    //inputの値をリセットする
+    setWord("");
     };
+
+    const onClickDelete =(e,i) => {
+    e.preventDefault()    
+    const newArr = [...arr];
+    newArr.splice(i, 1);
+    setArr(newArr);
+    console.log(newArr);
+    }
 
 
   return (
@@ -21,8 +33,8 @@ export const ToDoList = (props) => {
         <form>
         <input type="text" onChange={(e) => setWord( e.target.value)} value= {word} />        
         <button onClick={onClickAdd}>やるで！</button>
-        {arr.map((val)=>(<li>{(val)}< CompleteButton />
-        < DeleteButton /></li>))} 
+        {arr.map((val)=>(<li>{(val)} <button onClick={onClickDelete}>消すで！</button><button>やったで！</button></li>))} 
+        
         </form>        
       </div>
     
